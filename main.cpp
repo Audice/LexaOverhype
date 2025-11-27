a706a34e711af368b2ebde1d3203cef9cee21c1d
Audice <karchkov.denis@mail.ru>
2025-11-27 13:57:58 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a = -1;
+    std::cout << "Are you LexaOverhype? Input 1" << std::endl;
+    std::cin >> a;
+    if (a == 1) {
+        std::cout << "You are REALLI LexaOverhype!" << std::endl;
+    } else {
+        std::cout << "Fuk you!" << std::endl;
+    }
+    return 0;
+}
